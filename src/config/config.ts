ffaf8e54fcaf041ea0d8a8c2caca0d2c26113a19
kei711 <kei.yam.711@gmail.com>
2020-03-21 00:34:02 +0900

    Improve main.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,10 +1,8 @@
 type Config = {
-  port: number;
   nodeEnv: string;
 };
 
 const config: () => Config = () => ({
-  port: parseInt(process.env.PORT ?? '3000', 10),
   nodeEnv: process.env.NODE_ENV ?? 'local',
 });
 
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const configService = app.get<ConfigService>(ConfigService);
-  await app.listen(configService.get('port', 3000));
+  Logger.log('NODE_ENV: ' + configService.get('nodeEnv', 'local'), 'bootstrap');
+
+  await app.close();
 }
 bootstrap().catch(e => {
   throw e;
